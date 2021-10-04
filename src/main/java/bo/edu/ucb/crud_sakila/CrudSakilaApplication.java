162460b67187bd40b4ec7b1f07de2f3f3cda119a
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2021-10-04 19:37:25 -0400

    Inicio ejercicio sakila

---
+++ src/main/java/bo/edu/ucb/crud_sakila/CrudSakilaApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.crud_sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSakilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSakilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/crud_sakila/CrudSakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.crud_sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
